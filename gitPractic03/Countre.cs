d7e9de6c384c38a80dfcbe0657c832c5614762a4
Alex <lipenkov.a61@gmail.com>
2022-11-12 19:30:07 +0500

    commitA01

---
+++ gitPractic03/Countre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace GitPr;
+
+internal class Country
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите номер задания:");
+        var task = Convert.ToChar(Console.ReadLine()); 
+        switch (task)
+        {
+            case '1':
+                program.program();
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+        }
+    }
+}    
